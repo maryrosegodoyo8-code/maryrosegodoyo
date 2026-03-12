f95f369b07e9f1dc3813b4822edfd1753b874eb2
maryrosegodoyo8-code <maryrosegodoyo8@gmail.com>
2026-03-12 09:26:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "My updated Flask API!"
+    return "Welcome to my Flask API!"
+
+@app.route('/student')
+def student():
+    return "Student API is working!"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
-
